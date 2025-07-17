c851a1b034fa9b880f396e73622f08337713ec38
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-07-17 19:43:49 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FoodRecipeApp());
+}
+
+class FoodRecipeApp extends StatelessWidget {
+  const FoodRecipeApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
+
